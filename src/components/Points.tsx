fb4330e201b2da4cb25cd7f62913b7b0e8564d42
Kus Cámara <kcmr@users.noreply.github.com>
2022-12-11 13:15:09 +0100

    points styles

--- src/components/Points.tsx
+++ src/components/Points.tsx
@@ -1,9 +1,14 @@
 import { ComponentPropsWithoutRef } from 'react'
+import * as classes from './points.module.css'
 
 type PointProps = {
   total: number
-} & ComponentPropsWithoutRef<'span'>
+} & ComponentPropsWithoutRef<'p'>
 
 export const Points = ({ total, ...rest }: PointProps) => {
-  return <span {...rest}>{total} palabra(s)</span>
+  return (
+    <p {...rest} className={`${classes.points} ${rest.className ?? ''}`}>
+      <span className={classes.number}>{total}</span> palabra(s)
+    </p>
+  )
 }
