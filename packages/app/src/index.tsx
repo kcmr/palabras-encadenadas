0489e26c0863b7940b150aff287da049a28d0b7d
Kus Cámara <kcmr@users.noreply.github.com>
2023-01-17 22:24:01 +0100

    set html background color and bundle cookieconsent with main script

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import { App } from './components/app'
+import { setupCookieConset } from './setup-cookies'
+import 'vanilla-cookieconsent/dist/cookieconsent.css'
 import './animate.css'
 import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+
+window.addEventListener('load', setupCookieConset)
