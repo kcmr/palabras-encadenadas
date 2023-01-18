60b6679eafd1cfc543ed9bfb90d3a834c0042367
Kus Cámara <kcmr@users.noreply.github.com>
2023-01-18 23:46:59 +0100

    trigger build

--- packages/app/src/hooks/use-native-share.ts
+++ packages/app/src/hooks/use-native-share.ts
@@ -1,6 +1,10 @@
 export const useNativeShare = () => {
   const canUseNativeShare = 'share' in navigator
 
+  if (true) {
+    console.log('foo')
+  }
+
   /* istanbul ignore next */
   const share = (data?: ShareData) => {
     navigator.share(data).catch(() => {
